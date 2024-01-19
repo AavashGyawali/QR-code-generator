09f24e5b0ffaf03099ea9bc36d44398110db1239
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2024-01-19 09:48:30 +0545

    removed node packages

---
+++ index.js
@@ -0,0 +1,23 @@
+function generateQRCode() {
+  let website = document.getElementById("text").value;
+  if (website) {
+    // let qrcodeContainer = document.getElementById("qr_generator");
+    // qrcodeContainer.innerHTML = "";
+    // new QRCode(qrcodeContainer, website);
+    // document.getElementById("qr-text1").innerHTML = "Text: " + website;
+    // With some styles
+    let qrcodeContainer2 = document.getElementById("qr_generator");
+    qrcodeContainer2.innerHTML = "";
+    new QRCode(qrcodeContainer2, {
+      text: website,
+      width: 250,
+      height: 250,
+      colorDark: "#000000",
+      colorLight: "#e0e0e0",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+    // document.getElementById("qr_generator").style.display = "block";
+  } else {
+    alert("Please enter a valid URL");
+  }
+}
