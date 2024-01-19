634bf374ed1e690b716a1fc3854c2e29a12331a8
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2024-01-19 09:54:50 +0545

    size for container fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function generateQRCode() {
     // let qrcodeContainer = document.getElementById("qr_generator");
     // qrcodeContainer.innerHTML = "";
     // new QRCode(qrcodeContainer, website);
-    // document.getElementById("qr-text1").innerHTML = "Text: " + website;
+
     // With some styles
     let qrcodeContainer2 = document.getElementById("qr_generator");
     qrcodeContainer2.innerHTML = "";
@@ -16,6 +16,7 @@ function generateQRCode() {
       colorLight: "#e0e0e0",
       correctLevel: QRCode.CorrectLevel.H,
     });
+    document.getElementById("qr-text1").innerHTML = website;
     // document.getElementById("qr_generator").style.display = "block";
   } else {
     alert("Please enter a valid URL");
